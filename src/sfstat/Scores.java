7cb53a30ff4693191c47295d7c8f296b98d5be55
wadehuber <wadehuber@gmail.com>
2014-11-17 23:52:41 -0700

    Added initial Scores class.

---
+++ src/sfstat/Scores.java
@@ -0,0 +1,47 @@
+package sfstat;
+
+import java.util.Arrays;
+
+public class Scores {
+	
+	private String judge;
+	private int[] scores;
+	private int count;
+
+	
+	public Scores(String fileString, int scoreCount) {
+		String[] splitLine = fileString.split(",");
+		judge = splitLine[2];
+		scores = new int[scoreCount];
+		for(int ii=0;ii<scoreCount;ii++) {
+			scores[ii] = Integer.parseInt(splitLine[ii+3]); 
+		}
+	}
+	
+	public Scores(String judge, int[] scores) {
+		super();
+		this.judge = judge;
+		this.scores = scores;
+		count = scores.length;
+	}
+
+	
+	public String getJudge() {
+		return judge;
+	}
+	public void setJudge(String judge) {
+		this.judge = judge;
+	}
+	public int[] getScores() {
+		return scores;
+	}
+	public void setScores(int[] scores) {
+		this.scores = scores;
+	}
+	
+	public String toString() {
+		return "Scores [judge=" + judge + ", scores=" + Arrays.toString(scores)
+				+ "]";
+	}
+
+}
--- src/sfstat/sfstat.java
+++ src/sfstat/sfstat.java
@@ -1,10 +1,32 @@
 package sfstat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class sfstat {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello!");
+		Scanner dataFile;
+		String line;
+		String filename="sfdata.csv";
+		Scores tempScore;
+		
+		int ii = 0;
+		
+		try {
+			dataFile = new Scanner(new File(filename));
+			while (dataFile.hasNext()) {
+				line = dataFile.nextLine();
+				tempScore = new Scores(line, 20);
+				System.out.println("Line " + ii + ": " + tempScore);
+				ii ++;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error Reading file");
+		}
+
 
 	}
 
