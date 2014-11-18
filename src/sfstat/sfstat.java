86271d06a3807f7d9a585720617f08e5fc92c7eb
Mitch Jenkins <mitchellkjenkins@hotmail.com>
2014-11-18 12:41:13 -0500

    reformat

--- src/sfstat/sfstat.java
+++ src/sfstat/sfstat.java
@@ -4,10 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class sfstat {
-
-	public static void main(String[] args) {
-
+public class sfstat 
+{
+	public static void main(String[] args) 
+	{
 		Scanner dataFile;
 		String line;
 		String filename="sfdata.csv";
@@ -15,19 +15,20 @@ public class sfstat {
 		
 		int ii = 0;
 		
-		try {
+		try 
+		{
 			dataFile = new Scanner(new File(filename));
-			while (dataFile.hasNext()) {
+			while (dataFile.hasNext()) 
+			{
 				line = dataFile.nextLine();
 				tempScore = new Scores(line, 20);
 				System.out.println("Line " + ii + ": " + tempScore);
 				ii ++;
 			}
-		} catch (FileNotFoundException e) {
+		} 
+		catch (FileNotFoundException e) 
+		{
 			System.out.println("Error Reading file");
 		}
-
-
 	}
-
 }
