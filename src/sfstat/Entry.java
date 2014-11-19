547940e77042bed90229c3aec9ddb2d5830398c4
Mitch Jenkins <mitchellkjenkins@hotmail.com>
2014-11-19 01:11:31 -0800

    added Entry class

---
+++ src/sfstat/Entry.java
@@ -0,0 +1,19 @@
+package sfstat;
+
+public class Entry 
+{
+	private int entry;
+
+	public Entry(int entry)
+	{
+		setEntry(entry);
+	}
+	public void setEntry(int entry)
+	{
+		this.entry = entry;
+	}
+	public int getEntry()
+	{
+		return entry;
+	}
+}
