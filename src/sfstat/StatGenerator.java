68e8c4294f88db97b1601cb714cc1d88b605f661
wadehuber <wadehuber@gmail.com>
2014-11-20 14:20:12 -0700

    Added average calculation.

--- src/sfstat/StatGenerator.java
+++ src/sfstat/StatGenerator.java
@@ -15,6 +15,38 @@ public class StatGenerator {
 		scores.add(s);
 	}
 	
+	public double getAverageTotal(){
+		int total = 0;
+		
+		for (Scores s : scores) {
+			total += s.getTotal();
+		}
+		
+		return (double) total / (double) scores.size();
+	}	
+	
+	public double[] getAverageScores() {
+		
+		int numSubScores = scores.get(0).getCount();
+		double averages[] = new double[numSubScores];
+		for (int ii=0;ii<numSubScores;ii++) {
+			averages[ii]=0.0;
+		}
+
+		
+		for (Scores s : scores) {
+			int theseScores[] = s.getScores();
+			for (int ii=0;ii<numSubScores;ii++) {
+				averages[ii] += theseScores[ii];
+			}
+		}
+		
+		for (int ii=0;ii<numSubScores;ii++) {
+			averages[ii] /= (double) scores.size();
+		}
+		
+	    return averages;
+	}
 	
 	public String toString() {
 		String ret = "";
